2acb1881acb2ac5d399c74e5099273f22420d17f
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-10-25 04:58:00 +0000

    refactor: change option name/alias

--- src/index.ts
+++ src/index.ts
@@ -19,35 +19,35 @@ export interface IArguments extends Arguments {
 function builder(yargs: Argv) {
   return yargs
     .usage('Usage: $0 <build-dir> <zip-dir> [options]')
-    .positional('build-dir', {
+    .positional('buildDir', {
       describe: 'Directory of your build output',
       default: 'build'
     })
-    .positional('zip-dir', {
+    .positional('zipDir', {
       describe: 'Directory for your zipped backup',
       default: 'dist'
     })
-    .option('f', {
-      alias: 'format',
+    .option('format', {
+      alias: 'f',
       type: 'string',
       description: 'Format of output file',
       choices: formats,
       default: formats[0],
     })
-    .option('n', {
-      alias: 'name',
+    .option('name', {
+      alias: 'n',
       type: 'boolean',
       description: 'Ask for output archive filename',
       default: false,
     })
-    .option('t', {
-      alias: 'template',
+    .option('template', {
+      alias: 't',
       type: 'string',
       description: 'Template for output archive filename',
       default: '%NAME%_%VERSION%_%TIMESTAMP%.%EXT%',
     })
     .example('$0', "Zip 'build' directory and put archive under dist directory.")
-    .example('$0 out backup', "Zip 'out' directory and put archive under backup directory.")
+    .example('$0 out backup', "Zip 'out' directory and put archive under backup directory.");
 }
 
 yargs(hideBin(process.argv))
